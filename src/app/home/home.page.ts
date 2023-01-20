7da58dd35c1afa9ef926427ca559bb757c418993
Rodolphe <rodolphebansard@yahoo.com>
2023-01-20 15:04:51 +0100

    intial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
-import { RefresherCustomEvent } from '@ionic/angular';
+import {Component, OnInit} from '@angular/core';
+import {AlertController, RefresherCustomEvent} from '@ionic/angular';
 
 import { DataService, Message } from '../services/data.service';
+import {BluetoothSerial} from "@ionic-native/bluetooth-serial/ngx";
+
+interface Device {
+  class:number,
+  id:string,
+  address:string,
+  name:string
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  constructor(private data: DataService) { }
+export class HomePage implements OnInit{
+  devices: Device[] = [];
+  errorMessage:string=''
+  messageSent: string = '';
+  isConnected = false;
+  message:string='';
+
+  constructor(private data: DataService,
+              private bluetoothSerial: BluetoothSerial,
+              private alertController: AlertController) { }
 
   refresh(ev: any) {
     setTimeout(() => {
@@ -17,8 +33,41 @@ export class HomePage {
     }, 3000);
   }
 
-  getMessages(): Message[] {
-    return this.data.getMessages();
+
+  ngOnInit(): void {
+    this.checkBluetoothEnable();
   }
 
+  checkBluetoothEnable(){
+    this.bluetoothSerial.isEnabled().then((success) => {
+      this.listDevices();
+
+    },(error) => {
+      this.alertController.create({
+        header:'Error'
+      })
+      this.errorMessage= "bluetooth not enable"
+    })
+  }
+
+  listDevices(){
+    this.bluetoothSerial.list().then((success)=> {
+      this.devices = success;
+    },(error) => {
+      this.alertController.create({
+        header:'bluetooth error'
+      })
+      this.errorMessage="error while listing device"
+    })
+  }
+
+  connect(device:Device){
+    this.bluetoothSerial.connect(device.address).subscribe(()=> console.log('test'));
+  }
+
+  sendMessage(){
+    this.bluetoothSerial.write(this.message).then((success) => {
+      this.messageSent = this.message;
+    })
+  }
 }
